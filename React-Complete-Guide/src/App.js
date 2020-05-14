7407bd53581e4ba7f0ab869b27fcb344a2185d67
jandrooos <57966275+jandrooos@users.noreply.github.com>
2020-05-14 16:19:13 -0500

    Changed person list to dynamically display based on the objects in the array.

--- React-Complete-Guide/src/App.js
+++ React-Complete-Guide/src/App.js
@@ -5,22 +5,12 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Alex", age: 22 },
-      { name: "Tate", age: 22 },
-      { name: "Abril", age: 26 },
+      { id: "am123", name: "Alex", age: 22 },
+      { id: "tg123", name: "Tate", age: 22 },
+      { id: "fm123", name: "Abril", age: 26 },
     ],
     otherState: "some other value",
     showPersons: false,
-  };
-
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 21 },
-        { name: "Tatumn", age: 21 },
-        { name: "Fernanda", age: 25 },
-      ],
-    });
   };
 
   togglePersonsHandler = () => {
@@ -28,14 +18,27 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Alex", age: 21 },
-        { name: event.target.value, age: 21 },
-        { name: "Fernanda", age: 25 },
-      ],
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]; //ES7, the more modern way to copy the initial array
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   render() {
@@ -51,22 +54,17 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Jandro")} //This is the better way to pass an argument into switchNameHandler
-            changed={this.nameChangedHandler}
-          >
-            My Hobbies: Watching Netflix and Running
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
         </div>
       );
     }
