0e02112642b7837d05cd65a77af6737e0662ce2c
jandrooos <57966275+jandrooos@users.noreply.github.com>
2020-05-19 16:54:14 -0500

    added css module

--- React-Complete-Guide/src/App.js
+++ React-Complete-Guide/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -43,20 +42,8 @@ class App extends Component {
   };
 
   render() {
-    const styling = {
-      backgroundColor: "forestgreen",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-      ":hover": {
-        backgroundColor: "lightgreen",
-        color: "black",
-      },
-    };
-
     let persons = null;
+    let btnClass = [classes.Button];
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -73,27 +60,27 @@ class App extends Component {
           })}
         </div>
       );
-      styling.backgroundColor = "red";
-      styling[":hover"] = {
-        backgroundColor: "lightcoral",
-        color: "black",
-      };
+
+      btnClass.push(classes.Red);
     }
 
-    let classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
 
     //While it looks like HTML, it is not. It is instead JSX, made to look like HTML so you can write 'HTMLish' code.
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button className="button" onClick={this.togglePersonsHandler}>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
+        <button
+          className={btnClass.join(" ")}
+          onClick={this.togglePersonsHandler}
+        >
           Toggle Person
         </button>
         {persons}
